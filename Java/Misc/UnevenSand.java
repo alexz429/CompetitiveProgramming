137af52627dea0a375e5273407ecd8c95472a3e4
alexz429 <alexz2002429@gmail.com>
2019-05-13 17:59:15 -0400

    Update

---
+++ Java/Misc/UnevenSand.java
@@ -0,0 +1,32 @@
+package Misc;
+
+import java.util.Scanner;
+
+public class UnevenSand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		long small=1;
+		long big=2000000000;
+		long mid=1000000000;
+		System.out.println(mid);
+		String next=sc.next();
+		while(!next.equals("OK")) {
+
+			System.out.flush();
+			if(next.equals("FLOATS")) {
+				big=mid;
+				
+			}
+			else {
+				small=mid;
+				
+			}
+			mid=(small+big)/2;
+			System.out.println(mid);
+			next=sc.next();
+		}
+	}
+
+}
