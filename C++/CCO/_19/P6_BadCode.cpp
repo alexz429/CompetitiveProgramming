b4fc70c6a07ff2e9273381ba9747ebd4fd48a8d5
alexz429 <alexz2002429@gmail.com>
2020-04-18 18:55:27 -0400

    Update

--- C++/CCO/_19/P6_BadCode.cpp
+++ C++/CCO/_19/P6_BadCode.cpp
@@ -1,75 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int N,M;
-vector<pair<int,int>> dir[2550];
-ll hashes[50][50];
-int length[50];
+int N,M; string s[50]; vector<pair<int, pair<int,int>>> graph[51][51]; int visited[51][51];
+int ans = -1;
 // let node[x][i] be the representation of segment i of code x
 // x[i] -> y[j] implies a word can fit to bring the current position to y[j]
 // then djikstra or something I guess
 // since x,i<=50, combine node[x][i] into node[x*50+i] for easier stuff
-ll cut(int index, ll l, ll r){
-return hashes[index][r]&(1<<(r-l+1)-1);
-}
-void preprocess(int at){
-//unpack to x and i
-int x=at/51;
-int i=at%51;
-cout<<"HI "<<x<<" "<<i<<endl;
-int rem=length[x]-1-i;
-ll leadHash=cut(x,i,length[x]-1);
-for(int count=0;count<N;count++){
-    if(length[count]<=rem){
-        if(cut(x,i,i+length[count])==hashes[count][length[count]-1]){
-            dir[at].emplace_back(x*51+i+length[count],length[count]);
-            cout<<"Link from "<<x<<" "<<i<<"to "<<x<<" "<<i+length[count]<<endl;
-            if(i+length[count]!=length[x]-1){
-                cout<<"CALL"<<endl;
-                preprocess(x*51+i+length[count]);
-            }
+int main(){
+    for(int i=0;i<51;i++){
+        for(int j=0;j<51;j++){
+            visited[i][j] = 0x3f3f3f3f;
         }
     }
-    else{
-        ll compHash=hashes[count][rem];
-        if(compHash==leadHash){
-            cout<<x<<" "<<i<<" MATCHES "<<count<<rem<<endl;
-            ll newHash=cut(count,rem,length[count]-1);
-            
-            int newRem=length[count]-rem;
-            for(int count2=0;count2<N;count2++){
-                if(newHash==hashes[count2][newRem-1]){
-                    dir[at].emplace_back(count2*51+newRem-1,length[count]);
-                    cout<<"JUMP from "<<x<<" "<<i<<"to "<<count2<<" "<<newRem<<"USING "<<count<<endl;
-                    if(i+length[count]!=length[x]-1){
-                        cout<<"POP"<<endl;
-                        preprocess(count2*51+newRem-1);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>N>>M;
+    for(int i=0;i<N;i++){
+        cin>>s[i];
+    }
+    for(int i=0;i<N;i++){ 
+        for(int j=0;j<s[i].size();j++){
+            for(int k=0;k<N;k++){
+                if(i==k&&j==0)continue;
+                bool lim = true;
+                int limit = min(s[i].size()-j,s[k].size());
+                for(int l=0;l<limit;l++){
+                    if(s[i][l+j]!=s[k][l]){
+                        lim = false;
+                        break;
+                    }
+                }
+                if(lim){
+                    // cout<<i<<" "<<j<<" "<<k<<" "<<limit<<endl;
+                    if(limit + j == s[i].size()){
+                        graph[i][j].push_back({limit,{k,limit}});
+                    }
+                    else{
+                        graph[i][j].push_back({limit,{i,limit+j}});
                     }
                 }
             }
         }
     }
-}
-}
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin>>N>>M;
-    for(int count=0;count<N;count++){
-        string next;
-        cin>>next;
-        ll cur=0;
-        length[count]=next.length();
-        for(int i=0;i<next.length();i++){
-            cur*=2;
-            if(next[i]=='1'){
-                cur+1;
+    set<pair<int,pair<int,int>>> pqueue;
+    for(int i=0;i<N;i++){
+        pqueue.insert({0,{i,0}});
+    }
+    while(!pqueue.empty()){
+        pair<int,pair<int,int>> next = *pqueue.begin();
+        pqueue.erase(pqueue.begin());
+        // cout<<next.second.first<<" "<<next.second.second<<" "<<next.first<<" "<<pqueue.size()<<endl;
+        if(visited[next.second.first][next.second.second]<=next.first)continue;
+        visited[next.second.first][next.second.second] = next.first;
+        if(s[next.second.first].size()==next.second.second){
+            cout<<next.first<<endl;
+            return 0;
+        }
+        for(pair<int,pair<int,int>> connect:graph[next.second.first][next.second.second]){
+            if(visited[connect.second.first][connect.second.second]>visited[next.second.first][next.second.second]+connect.first){
+            // cout<<"link"<<connect.first<<" "<<connect.second.first<<" "<<connect.second.second<<endl;
+                pqueue.insert({visited[next.second.first][next.second.second]+connect.first,{connect.second.first,connect.second.second}}); 
             }
         }
     }
-    for(int count=0;count<N;count++){
-        preprocess(count*51);
-    }
-
-
+    cout<<-1<<"\n";
 }
